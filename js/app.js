2b9e2d0982092e1e73c135b5d1b650eb12c52382
coderrakibul <coder.rakibul@gmail.com>
2022-02-17 15:26:49 +0600

    calculate validation is added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,77 @@
+// // calculate part is here
+// document.getElementById("calculate-btn").addEventListener("click", function(){
+
+//     // get the input fields
+//     const incomeField = document.getElementById("income-field");
+//     const foodField = document.getElementById("food-field");
+//     const rentField = document.getElementById("rent-field");
+//     const clothField = document.getElementById("cloth-field");
+//     const totalExpense = document.getElementById("total-expense");
+//     const totalBalance = document.getElementById("total-balance");
+
+
+//     //calculate expenses and balance
+//     const totalExpenseAmount = parseFloat(totalExpense.innerText) + parseFloat(foodField.value) + parseFloat(rentField.value) + parseFloat(clothField.value);
+//     totalExpense.innerText = totalExpenseAmount;
+
+//     foodField.value = "";
+//     rentField.value = "";
+//     clothField.value = "";
+
+//     const totalBalanceAmount = parseFloat(incomeField.value) - parseFloat(totalExpenseAmount);
+//     totalBalance.innerText = totalBalanceAmount;
+    
+// })
+
+// // money saving part is here
+// document.getElementById("save-btn").addEventListener("click", function(){
+//    // get the input fields
+//     const savingField = document.getElementById("saving-field");
+//     const savingTotal = document.getElementById("saving-Total");
+//     const remainingBalance = document.getElementById("remaining-balance");
+//     const incomeField = document.getElementById("income-field");
+//     const totalBalance = document.getElementById("total-balance");
+
+//     //calculate the saving and remaining balance
+//     const savingFieldInput = savingField.value;
+//     const savingAmount = (incomeField.value / 100) * savingFieldInput;
+
+//     savingTotal.innerText = savingAmount;
+
+//     incomeField.value = "";
+//     savingField.value = "";
+
+//     const remainingBalanceAmount = parseFloat(totalBalance.innerText) - parseFloat(savingAmount);
+//     remainingBalance.innerText = remainingBalanceAmount;
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//code test
 // calculate part is here
 document.getElementById("calculate-btn").addEventListener("click", function(){
 
@@ -11,6 +85,10 @@ document.getElementById("calculate-btn").addEventListener("click", function(){
 
 
     //calculate expenses and balance
+    if(isNaN(incomeField.value) || incomeField.value < 0 || incomeField.value == "" || isNaN(foodField.value) || foodField.value < 0 || foodField.value == ""  || isNaN(rentField.value) || rentField.value < 0 || rentField.value =="" || isNaN(clothField.value) || clothField.value < 0 || clothField.value =="" ){
+        incomeField.value = "";
+        return alert("please insert number")
+    }
     const totalExpenseAmount = parseFloat(totalExpense.innerText) + parseFloat(foodField.value) + parseFloat(rentField.value) + parseFloat(clothField.value);
     totalExpense.innerText = totalExpenseAmount;
 
@@ -44,3 +122,5 @@ document.getElementById("save-btn").addEventListener("click", function(){
     const remainingBalanceAmount = parseFloat(totalBalance.innerText) - parseFloat(savingAmount);
     remainingBalance.innerText = remainingBalanceAmount;
 })
+
+
